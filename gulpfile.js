a55cc41ee9ff20b72958281e8006d2aec553c666
Aamer94 <am94.sy.no@gmail.com>
2022-06-09 09:11:23 +0200

    header-component

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const gulp = require('gulp');
 const gulpsass = require('gulp-sass');
 const sass = gulpsass(require('sass'));
 
-gulp.task('sass',async function(){
-    gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+gulp.task('sass', async function () {
+    gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
 })
